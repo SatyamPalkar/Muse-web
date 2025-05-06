ce357ff131dee32da4659ea3c1d0c29b8f1bdb79
Satyam Palkar <73163418+SatyamPalkar@users.noreply.github.com>
2025-05-06 19:12:01 +1000

    replaced the websocket confirguration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+"use client"
+
+import { useWebSocket } from "@/contexts/websocket-context"
 import { WelcomeScreen } from "@/components/welcome-screen"
+import { useEffect } from "react"
 
 export default function Home() {
+  const { currentData, connected } = useWebSocket()
+
+  useEffect(() => {
+    if (connected && currentData) {
+      console.log("📡 EEG Data Received:", currentData)
+    }
+  }, [currentData, connected])
+
   return <WelcomeScreen />
 }
